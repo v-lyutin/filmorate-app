14eb371f820ceb2edba999b073c49bc78b591d5b
v-lyutin <dyson-sv25@yandex.ru>
2023-12-17 21:57:20 +0300

    feat: initial commit

---
+++ src/main/java/com/filmorate/filmorateapp/FilmorateAppApplication.java
@@ -0,0 +1,13 @@
+package com.filmorate.filmorateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmorateAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmorateAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/filmorate/filmorateapp/FilmorateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmorate.filmorateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmorateAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
